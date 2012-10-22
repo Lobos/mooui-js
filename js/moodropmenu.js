804e881cb2282d60577ab25698d75266ba82d97f
lobos <lobos841@gmail.com>
2012-10-22 17:33:18 +0800

    moodropmenu 改为click

--- js/moodropmenu.js
+++ js/moodropmenu.js
@@ -23,13 +23,13 @@ var MooDropMenu = new Class({
 
 	options: {
 		onOpen: function(el){
-			el.removeClass('close').addClass('open');
+			el.addClass('open');
 		},
 		onClose: function(el){
-			el.removeClass('open').addClass('close');
+			el.removeClass('open');
 		},
 		onInitialize: function(el){
-			el.removeClass('open').addClass('close');
+			el.removeClass('open');
 		},
 		mouseoutDelay: 200,
 		mouseoverDelay: 0,
@@ -50,27 +50,61 @@ var MooDropMenu = new Class({
 			this.fireEvent('initialize', el);
 
 			var parent = el.getParent(options.itemSelector),
-				timer;
+                self = this,
+                _k = 'DropDownOpen';
 
-			parent.addEvent(options.openEvent, function(){
+            parent.addEvent('click', function () {
+                if (parent.retrieve(_k)) return;
+                var selector = el.getParent(options.itemSelector);
+
+                var _close = function (event) {
+                    if (event.target == selector.getElement('a'))
+                        return;
+                    self.fireEvent('close', selector);
+                    parent.store(_k, false);
+                    document.id(document.body).removeEvent('click', _close);
+                };
+                document.id(document.body).addEvent('click', _close);
+                parent.store(_k, true);
+                this.fireEvent('open', selector);
+            }.bind(this));
+
+			/*parent.addEvent(options.openEvent, function(){
 				parent.store('DropDownOpen', true);
 
 				clearTimeout(timer);
 				if (options.mouseoverDelay) timer = this.fireEvent.delay(options.mouseoverDelay, this, ['open', el]);
-				else this.fireEvent('open', el);
+				else this.fireEvent('open', el.getParent(options.itemSelector));
 
 			}.bind(this)).addEvent(options.closeEvent, function(){
 				parent.store('DropDownOpen', false);
 
 				clearTimeout(timer);
 				timer = (function(){
-					if (!parent.retrieve('DropDownOpen')) this.fireEvent('close', el);
+					if (!parent.retrieve('DropDownOpen')) this.fireEvent('close', el.getParent(options.itemSelector));
 				}).delay(options.mouseoutDelay, this);
 
-			}.bind(this));
+			}.bind(this));*/
 
 		}, this);
 	},
+
+    close: function () {
+        this.menu.getElements(this.options.itemSelector).removeClass('open');
+    },
+
+    open: function (el) {
+        var self = this;
+        var _close = function (event) {
+            if (el.contains(event.target))
+                return;
+            self.fireEvent('close', el);
+            document.body.removeEvent('click', _close);
+        };
+        document.body.addEvent('click', _close);
+        parent.store('DropDownOpen', true);
+        this.fireEvent('open', el);
+    },
 
 	toElement: function(){
 		return this.menu
