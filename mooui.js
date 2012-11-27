2d3d6991c474deedd4d36db6d61c708a3a817919
lobos <lobos841@gmail.com>
2012-11-27 14:52:52 +0800

    form

--- mooui.js
+++ mooui.js
@@ -67,6 +67,51 @@ MooUI.Global = new new Class({
         }, options);
 
         new MooUI.Openbox(options).open();
+    },
+
+    getFV: function (form, options) {
+        options = Object.merge({
+            evaluateOnBlur: false,
+            stopOnFailure: false,
+            onSuccess: function (el) {
+                var parent = el.getParent('.control-group');
+                parent.removeClass('error');
+                var help = parent.getElement('[class^=help-]');
+                if (help) {
+                    help.set('html', '<i class="icon-ok"></i>');
+                }
+            },
+            onFailure: function (el, msg) {
+                var parent = el.getParent('.control-group');
+                parent.addClass('error');
+                var help = parent.getElement('[class^=help-]');
+                if (help) {
+                    help.set('html', msg);
+                }
+            }
+        }, options);
+
+        return new MooUI.Validator(form, options);
+    },
+
+    form: function (form, fv, options) {
+        form = document.id(form);
+        var btn = form.getElement('[type="submit"]');
+        options = Object.merge({
+            url: form.get('action'),
+            method: form.get('method'),
+            onComplete: function () {
+                if (btn) btn.removeProperty('disabled');
+            }
+        }, options);
+        form.addEvent('submit', function (event) {
+            event.preventDefault();
+            var suc = true;
+            if (fv) fv.validate();
+            if (!suc) return false;
+            if (btn) btn.set('disabled', 'disabled');
+            new Request.JSON(options).send(form);
+        });
     }
 });
 
