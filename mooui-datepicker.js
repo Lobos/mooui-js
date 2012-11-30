016c3f060e4b7a7078bb779ef690d8f8e3254908
lobos <lobos841@gmail.com>
2012-11-30 17:10:32 +0800

    datepicker 基本完成

--- mooui-datepicker.js
+++ mooui-datepicker.js
@@ -23,12 +23,16 @@ MooUI.DatePicker = new Class({
 
     options: {
         destroyOnClose: true,
+        bind: null,
+        format: '%Y-%m-%d', //mootools Date format
         css: {
             picker:             'date-picker',
             header:             'date-header',
             nextMonth:          'icon-metro-right icon-large right',
             previousMonth:      'icon-metro-left icon-large',
-            active:             'active'
+            active:             'active',
+            yearSelect:         'year-select unstyled',
+            monthSelect:        'month-select unstyled'
         }
     },
 
@@ -42,10 +46,11 @@ MooUI.DatePicker = new Class({
         this.create();
 
         this.currentDay = new Date();
+        if (this.options.bind) this.bind(this.options.bind);
     },
 
     create: function () {
-        this.canvas = document.id(this.options.container) || document.body;
+        this.canvas = document.id(this.options.canvas) || document.body;
         var picker = this.picker = new Element('div', {
             'class': this.css.picker
         }).inject(this.canvas);
@@ -57,15 +62,23 @@ MooUI.DatePicker = new Class({
 
         var _changeMonth = function (m) {
             var date = new Date(this.yearHandle.retrieve('year'), this.monthHandle.retrieve('month') + m, 1);
-            this.draw(date.getFullYear(), date.getMonth());
+            this.drawDay(date.getMonth(), date.getFullYear());
         }.bind(this);
 
         new Element('i', {
             'class': this.css.previousMonth,
             events: {
                 click: function () {
-                    _changeMonth(-1);
-                }
+                    switch (this.stage) {
+                        case 'year':
+                            var y = this.yearHandle.retrieve('year') - 25;
+                            this.drawYear(y);
+                            break;
+                        default:
+                            _changeMonth(-1);
+                            break;
+                    }
+                }.bind(this)
             }
         }).inject(header);
 
@@ -73,32 +86,105 @@ MooUI.DatePicker = new Class({
             'class': this.css.nextMonth,
             events: {
                 click: function () {
-                    _changeMonth(1);
-                }
+                    switch (this.stage) {
+                        case 'year':
+                            var y = this.yearHandle.retrieve('year') + 25;
+                            this.drawYear(y);
+                            break;
+                        default:
+                            _changeMonth(1);
+                            break;
+                    }
+                }.bind(this)
             }
         }).inject(header);
 
         this.yearHandle = new Element('a', {
-            href: 'javascript:;'
+            href: 'javascript:;',
+            events: {
+                click: function () {
+                    this.drawYear();
+                }.bind(this)
+            }
         }).inject(header);
 
         this.monthHandle = new Element('a', {
-            href: 'javascript:;'
+            href: 'javascript:;',
+            events: {
+                click: this.drawMonth.bind(this)
+            }
         }).inject(header);
 
-
+        //create calendar
         this.calendar = new Element('div').inject(picker);
     },
 
-    bind: function (input) {
-        input = document.id(input);
+    bind: function (inputs) {
         var self = this;
-        input.addEvent('click', function () {
-            self.show(this.get('value'));
+        $$(inputs).addEvent('click', function () {
+            self.bindInput = this;
+            self.open.delay(5, self, [this.get('value')]);
         });
+        return this;
     },
 
-    draw: function (year, month) {
+    drawYear: function (year) {
+        var ul = new Element('ul', {
+            'class': this.css.yearSelect
+        });
+
+        if (year)
+            this.yearHandle.set('html', _gl('year').format(year)).store('year', year);
+        else
+            year = this.yearHandle.retrieve('year');
+
+        year = year - 12 < 0 ? 0 : year - 12;
+        (25).times(function (i) {
+            var y = year + i;
+            new Element('li', {
+                html: y,
+                events: {
+                    click: function () {
+                        this.yearHandle.set('html', _gl('year').format(y)).store('year', y);
+                        this.drawMonth();
+                    }.bind(this)
+                }
+            }).inject(ul);
+        }.bind(this));
+
+        this.calendar.empty().grab(ul);
+        this.stage = 'year';
+
+        if (this.showAbove) this.position();
+        return this;
+    },
+
+    drawMonth: function () {
+        var ul = new Element('ul', {
+            'class': this.css.monthSelect
+        });
+        (12).times(function (i) {
+            var m = _gl('months')[i];
+            new Element('li', {
+                html: m,
+                events: {
+                    click: function () {
+                        this.monthHandle.set('html', m).store('month', i);
+                        this.drawDay(i);
+                    }.bind(this)
+                }
+            }).inject(ul);
+        }.bind(this));
+        this.calendar.empty().grab(ul);
+        this.stage = 'month';
+
+        if (this.showAbove) this.position();
+        return this;
+    },
+
+    drawDay: function (month, year) {
+        if (month == undefined) month = this.monthHandle.retrieve('month');
+        year = year || this.yearHandle.retrieve('year');
         var start = new Date(year, month, 1),
             index = 1 - start.getDay(),
             max = new Date(year, month + 1, 0).getDate(),
@@ -128,10 +214,16 @@ MooUI.DatePicker = new Class({
             (7).times(function () {
                 if (index > 0 && index <= max) {
                     var td = new Element('td').inject(tr);
+                    var day = index;
                     new Element('a', {
                         href: 'javascript:;',
-                        'class': index == currentDay ? this.css.active : '',
-                        html: index
+                        'class': day == currentDay ? this.css.active : '',
+                        html: day,
+                        events: {
+                            click: function () {
+                                this.selectDay(day);
+                            }.bind(this)
+                        }
                     }).inject(td);
                 } else {
                     new Element('td', {
@@ -144,25 +236,73 @@ MooUI.DatePicker = new Class({
 
         this.calendar.empty().grab(table);
         table.fade('hide').fade('in');
+        this.stage = 'day';
 
+        if (this.showAbove) this.position();
         return this;
     },
 
-    show: function (date) {
+    selectDay: function (day) {
+        var date = new Date(this.yearHandle.retrieve('year'), this.monthHandle.retrieve('month'), day);
+        this.bindInput.set('value', date.format(this.options.format));
+        this.close();
+    },
+
+    open: function (date) {
+        this.picker.show();
+
         date = date ? new Date.parse(date) : new Date();
         var year = date.getFullYear(),
             month = date.getMonth();
 
         this.currentDay = date;
-        this.draw(year, month);
+        this.drawDay(month, year);
+        this.checkAbove().position();
+        this.isOpen = true;
+
+        var _close = function (event) {
+            if (this.picker.contains(event.target)) return;
+            this.close();
+            document.removeEvent('click', _close);
+        }.bind(this);
+
+        document.addEvent.delay(10, document, ['click', _close]);
+    },
+
+    checkAbove: function () {
+        var pos = this.bindInput.getPosition(this.canvas),
+            bws = this.canvas == document.body ? { x:0, y:0 } : this.canvas.getScroll(),
+            top = pos.y + this.bindInput.outerHeight() - bws.y + 3,
+            h = this.picker.outerHeight();
+        h = h > 300 ? h : 300;
+        this.showAbove = top + h > this.canvas.getSize().y + this.canvas.getScroll().y;
+        return this;
+    },
+
+    position: function () {
+        var pos = this.bindInput.getPosition(this.canvas),
+            bws = this.canvas == document.body ? { x:0, y:0 } : this.canvas.getScroll(),
+            left = pos.x + bws.x,
+            top = pos.y + this.bindInput.outerHeight() - bws.y + 3;
+
+        if (this.showAbove)
+            top = pos.y - this.picker.outerHeight() + bws.y - 3;
+
+        this.picker.setStyles({
+            'left': left,
+            'top': top,
+            'z-index': Object.topZIndex()
+        });
+
     },
 
     close: function () {
-
+        this.isOpen = false;
+        this.picker.hide();
     },
 
     destroy: function () {
-
+        this.picker.destroy();
     }
 });
 
